641b0eba51fa1696d1ab5b478abe0efd0a8b8987
nhill <nhill@MININT-CU56C8E>
2020-06-15 10:14:21 +0100

    added trainer console app

---
+++ placebo/Placebo.Trainer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Placebo.Trainer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
